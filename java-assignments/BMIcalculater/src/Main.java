c8c267496a7d95baf8df504a30c2ee50a5170b43
cansev <mee.cansev@gmail.com>
2023-09-13 18:16:42 +0300

    add: BMI calculater program file directory

---
+++ java-assignments/BMIcalculater/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vücut Kitle İndeksi (BMI) Hesaplama Programı");
+        System.out.print("Kilonuzu (kg) giriniz: ");
+        double kilo = scanner.nextDouble();
+
+        System.out.print("Boyunuzu (metre cinsinden) giriniz: ");
+        double boy = scanner.nextDouble();
+
+        double bmi = kilo / (boy * boy);
+
+        System.out.println("BMI Değeriniz: " + bmi);
+
+    }
+}
