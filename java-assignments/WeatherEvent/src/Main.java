cfdb2e61a364d7e0a0c5d119bed382d0732c3b3e
cansev <mee.cansev@gmail.com>
2023-09-16 17:01:38 +0300

    add: week-1 WeatherEvent file directory

---
+++ java-assignments/WeatherEvent/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int weather;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Hava Sıcaklığını giriniz :");
+        weather = input.nextInt();
+
+        if (weather > 5) {
+            System.out.println("Kayak Yapabilirsiniz.");
+        } else if (weather > 5 && weather >= 15) {
+            System.out.println("sinemaya gidebilirsiniz.");
+
+        }else if (weather > 15 && weather >= 25) {
+            System.out.println("pikniğe gidebilirsiniz.");
+
+        }
+
+    }
+}
