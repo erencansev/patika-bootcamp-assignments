c5c653a5e64fb3a0799f3bc7ec364744ee573df1
cansev <mee.cansev@gmail.com>
2023-09-16 13:09:00 +0300

    add: week-1 KDV-calculater file directory

---
+++ java-assignments/KDVcalculater/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       double tutar, kdvOrani = 0.18;
+
+
+       Scanner input = new Scanner(System.in);
+        System.out.println("ücret tutarını girin: ");
+
+        tutar = input.nextDouble();
+
+       double kdvTutari = tutar * kdvOrani;
+       double kdvliTutar = tutar + kdvTutari ;
+
+
+        System.out.println("kdv oranı :" + kdvOrani);
+        System.out.println("kdv tutarı :" + kdvTutari);
+        System.out.println("kdvli tutar :" + kdvliTutar);
+
+    }
+}
