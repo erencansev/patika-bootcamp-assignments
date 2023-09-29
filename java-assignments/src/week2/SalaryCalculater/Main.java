9c77b61baf21935217fb4bf2221311da558a5991
cansev <mee.cansev@gmail.com>
2023-09-29 11:29:51 +0300

    feat: week-2 SalaryCalc file directory

---
+++ java-assignments/src/week2/SalaryCalculater/Main.java
@@ -0,0 +1,9 @@
+package week2.SalaryCalculater;
+
+public class Main {
+        public static void main(String[] args) {
+            Employee memati = new Employee("Memati Baş", 2000, 45, 1985);
+            System.out.println(memati.toString());
+        }
+    }
+
