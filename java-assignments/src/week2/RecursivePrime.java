5a0188090addef5ac8756b7090c54ad36de70719
cansev <mee.cansev@gmail.com>
2023-09-29 11:13:55 +0300

    feat: week-2 RecursivePrime file directory added

---
+++ java-assignments/src/week2/RecursivePrime.java
@@ -0,0 +1,29 @@
+package week2;
+
+
+import java.util.Scanner;
+
+public class RecursivePrime {
+    static int checkPrime(int number, int number2) {
+        if(number2==1) {
+            return 0;
+        }
+        if(number % number2 == 0) {
+            return -1;
+        } else {
+            return checkPrime(number, number2-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kontrol edilecek sayıyı giriniz: ");
+        int number = input.nextInt();
+        int result = checkPrime(number, number-1);
+        if(result==0)
+            System.out.println(number+" asal sayıdır ");
+        else
+            System.out.println(number+" asal sayı değildir ");
+    }
+
+}
