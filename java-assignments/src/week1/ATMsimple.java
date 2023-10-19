76c51c9aa9300a17dc80ef57b33d985e820126ee
cansev <mee.cansev@gmail.com>
2023-10-19 09:34:46 +0300

    feat: week-1 atm project added

---
+++ java-assignments/src/week1/ATMsimple.java
@@ -0,0 +1,67 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ATMsimple {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int deneme = 3, bakiye = 10000, islem;
+        while(deneme>0) {
+            System.out.print("Kullanıcı adınızı giriniz: ");
+            String kullaniciAdi = input.nextLine();
+            System.out.print("Şifrenizi giriniz: ");
+            String sifre = input.nextLine();
+            if(kullaniciAdi.equals("erencansev") && sifre.equals("2002")) {
+                System.out.println("Merhaba, TD Bank'a hoşgeldiniz!");
+                do {
+                    System.out.println("Yapmak istediğiniz işlemi seçiniz");
+                    islem = input.nextInt();
+                    switch (islem) {
+                        case 1:
+                            System.out.println("Para çekme");
+                            System.out.print("Çekmek istediğiniz tutarı giriniz: ");
+                            int cekilenTutar = input.nextInt();
+                            if(cekilenTutar > bakiye) {
+                                System.out.println("Yetersiz bakiye!");
+                            }
+                            else {
+                                bakiye -= cekilenTutar;
+                                System.out.println("Güncel bakiyeniz: "+bakiye);
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Para yatırma");
+                            System.out.print("Yatırmak istediğiniz tutarı giriniz:");
+                            int yatirilanTutar = input.nextInt();
+                            bakiye += yatirilanTutar;
+                            System.out.println("Güncel bakiyeniz: "+bakiye);
+                            break;
+                        case 3:
+                            System.out.println("Güncel bakiyeniz: "+bakiye);
+                            break;
+                        case 4:
+                            System.out.println("Sistemden çıkış yapıldı!");
+                            break;
+                        default:
+                            System.out.println("Hatalı tuşlama yapıldı!");
+                    }
+                }
+                while(islem != 4);
+                break;
+            }
+            else {
+                deneme--;
+                System.out.println("Kullanıcı adı veya şifreniz hatalıdır!");
+                if(deneme == 0) {
+                    System.out.println("Hesabınız bloke olmuştur, lütfen bankanızla iletişime geçiniz!");
+                }
+                else {
+                    System.out.println("Kalan hakkınız: "+deneme);
+                }
+            }
+        }
+
+    }
+
+}
