34de32ad8049e7b6e37800c27f687e5ce4caf91a
cansev <mee.cansev@gmail.com>
2023-10-23 02:06:55 +0300

    feat: week-2 sort numbers file added

---
+++ java-assignments/src/week2/SortNumbers.java
@@ -0,0 +1,30 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutu n: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Dizinin elemanlarını giriniz:");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + ". Elemanı: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print("Sıralama: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
